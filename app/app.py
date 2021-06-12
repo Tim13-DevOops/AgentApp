f8053c9d1caaba3649e124676225d2dd0e70e0c8
Dusan Milunovic <dusanmilunovic17@gmail.com>
2021-06-12 18:35:45 +0200

    Return CSRF

--- app/app.py
+++ app/app.py
@@ -7,10 +7,10 @@ import app.config as config
 from app.repository.database import init_database
 
 app = Flask(__name__)
-# app.config['SECRET_KEY'] = config.FLASK_SECRET_KEY
+app.config['SECRET_KEY'] = config.FLASK_SECRET_KEY
 
-# csrf = CSRFProtect(app)
-# csrf.init_app(app)
+csrf = CSRFProtect(app)
+csrf.init_app(app)
 
 api = Api(app)
 db = init_database(app)
