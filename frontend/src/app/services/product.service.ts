17a90d5aa585c061f589a2eedc7b53f4c67f1acb
bandjeo <branislav.andjelic@gmail.com>
2021-06-20 17:06:20 +0200

    Create frontend components for product #2

--- frontend/src/app/services/product.service.ts
+++ frontend/src/app/services/product.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Product } from '../models/product-model';
 
 import { map } from 'rxjs/operators';
 
@@ -13,9 +14,34 @@ export class ProductService {
 
   constructor(private http: HttpClient) { }
 
-  get(): Observable<any> {
-    return this.http.get(baseURL).pipe(map(result => {
-      return result
+  get(): Observable<Product[]> {
+    return this.http.get(baseURL).pipe(map((result: any) => {
+      return result.map(item => new Product(item))
+    }))
+  }
+
+  getOne(id: number): Observable<Product> {
+    return this.http.get(`${baseURL}/${id}`).pipe(map((result: any) => {
+      return new Product(result)
+    }))
+  }
+
+  post(product: Product): Observable<Product> {
+    return this.http.post(baseURL, product).pipe(map((result: any) => {
+      return new Product(result)
+    }))
+  }
+
+  put(product: Product): Observable<Product> {
+    return this.http.put(baseURL, product).pipe(map((result: any) => {
+      return new Product(result)
+    }))
+  }
+
+  
+  delete(id: number): Observable<Product> {
+    return this.http.delete(`${baseURL}/${id}`).pipe(map((result: any) => {
+      return new Product(result)
     }))
   }
 }
